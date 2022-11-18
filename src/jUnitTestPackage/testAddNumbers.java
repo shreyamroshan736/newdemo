6f122551f7d38997d85f1cd95d04cab9d42763cc
shrey <shrey@LAPTOP-ITCL4DF6>
2022-11-18 11:40:29 +0530

    proj shreyam

---
+++ src/jUnitTestPackage/testAddNumbers.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/jUnitTestPackage/testAddNumbersTest.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbersTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
